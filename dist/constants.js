ad4d8f4bf8ac869abf55cc7ca92ac4574db37e02
Chris Pomerantz <cpomer@yahoo.com>
2026-02-27 18:43:09 -0800

    Add .gitignore, remove build artifacts and node_modules

--- dist/constants.js
+++
@@ -1,9 +0,0 @@
-export const CATEGORIES = ['people', 'projects', 'ideas', 'admin'];
-export const INBOX_STATUSES = ['filed', 'needs_review', 'fixed', 'error', 'dismissed'];
-export const CONFIDENCE_THRESHOLDS = {
-    /** Above this, auto-file without review */
-    AUTO_FILE: 0.85,
-    /** Below this, hold for human review */
-    NEEDS_REVIEW: 0.6,
-};
-//# sourceMappingURL=constants.js.map
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-export * from './constants.js';
-export * from './schemas.js';
-export * from './types.js';
-export { uuidv7 } from 'uuidv7';
-//# sourceMappingURL=index.js.map
